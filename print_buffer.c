3bc0a3c155ba8b786e908de611503fd3579d0850
NourBahgat <nourbahgat2003@gmail.com>
2022-10-18 16:43:16 -0700

    0

---
+++ print_buffer.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * print_buffer - writes the character c
+ * @s: The characters to print
+ * @size: size
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+int print_buffer(char *s, int size)
+{
+if (size > 0)
+return (write(1, s, size));
+return (-1);
+}
