934b3b987e1433827ecfacac9dee11da6b3be2b8
Mo2Hefny <moamensaied1@gmail.com>
2022-10-18 18:50:52 +0200

    fixing

--- check_conversion.c
+++ check_conversion.c
@@ -2,56 +2,58 @@
 
 /**
  * check_conversion - check conversion letter after %
- * @op: conversion options in format
- * @args: arguments in function
- * @index: number of conversion to check in op array
+ * @c: letter
  * Return: size
  */
 
 int check_conversion(char *op, va_list args, int index)
 {
-char *s = NULL, c;
-int l = 1, i, n;
-for (i = 0; i <= index && op[i]; i++)
-{
-switch (*(op + i))
-{
-case 'c':
-c = va_arg(args, int);
-if (i == index)
-return (print_buffer(&c, &l));
-break;
-case 's':
-s = va_arg(args, char *);
-if (i == index)
-return (print_str(s));
-break;
-case 'd':
-case 'i':
-n = va_arg(args, int);
-if (i == index)
-return (print_dec(n));
-break;
-case 'x':
-case 'X':
-n = va_arg(args, int);
-if (i == index)
-return (print_hex(n, *(op + i)));
-break;
-case 'b':
-n = va_arg(args, int);
-if (i == index)
-return (print_binary(n));
-break;
-case '%':
-print_buffer("%", &l);
-return (1);
-default:
-print_buffer("%", &l);
-print_buffer(*(op + i), &l);
-return (-1);
-}
-}
+  char *s = NULL, c;
+  int i, n;
+  for (i = 0; i <= index && op[i]; i++)
+  {
+    switch (*(op + i))
+    {
+    case 'c':
+      c = va_arg(args, int);
+      if (i == index)
+      {
+      print_buffer(&c, 1);
+        return (1);
+    }
+      break;
+    case 's':
+      s = va_arg(args, char *);
 
-return (-1);
+       if (i == index)
+        return (print_str(s));
+      break;
+    case 'i':
+    case 'd':
+      n = va_arg(args, int);
+      if (i == index)
+        return (print_dec(n));
+      break;
+    case 'x':
+    case 'X':
+      n = va_arg(args, int);
+      if (i == index)
+        return (print_hex(n, *(op + i)));
+      break;
+    case 'b':
+      n = va_arg(args, unsigned int);
+      if (i == index)
+        return (print_binary(n));
+      break;
+    case '%':
+      print_buffer("%", 1);
+      return (1);
+    default:
+      print_buffer("%", 1);
+      print_buffer((op + i), 1);
+      return (2);
+    }
+  }
+
+  return (0);
 }
